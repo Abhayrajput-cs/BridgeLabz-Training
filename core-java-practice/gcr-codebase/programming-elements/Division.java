bf9e33f2e287ae2a4bbb36e9d4dc53f8d62124bd
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2025-12-18 14:53:28 +0530

    Level2 2nd

---
+++ core-java-practice/gcr-codebase/programming-elements/Division.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Division{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int number1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int number2 = sc.nextInt();
+
+        int quotient = number1 / number2;   // Division operator
+        int remainder = number1 % number2;  // Modulus operator
+
+        System.out.println(
+                "The Quotient is " + quotient +
+                " and Reminder is " + remainder +
+                " of two number " + number1 +
+                " and " + number2
+        );
+
+        sc.close();
+    }
+}
