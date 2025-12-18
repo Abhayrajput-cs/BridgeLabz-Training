5bc8564a8e62cc991ea40c0b15780a92ddb5e16b
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2025-12-18 10:17:42 +0530

    day3

---
+++ core-java-practice/gcr-codebase/programming-elements/add2num.java
@@ -0,0 +1,9 @@
+
+public class add2num {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
+}
