1db350ec3efcf8fc7de7ca2ee0644e51693d094d
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2025-12-18 10:57:48 +0530

    11th

---
+++ core-java-practice/gcr-codebase/programming-elements/AreaOfCircle.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class AreaOfCircle {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double radius = in.nextDouble();
+        double area = Math.PI * radius * radius;
+        System.out.println("Area of circle with radius " + radius + " is: " + area);
+    }
+}
