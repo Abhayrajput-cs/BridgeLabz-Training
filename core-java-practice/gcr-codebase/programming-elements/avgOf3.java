28e97858dac5e0b3d6729b8ef43f4615f9ff0154
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2025-12-18 12:39:11 +0530

    8th

---
+++ core-java-practice/gcr-codebase/programming-elements/avgOf3.java
@@ -0,0 +1,11 @@
+
+public class avgOf3 {
+public static void main(String args[])
+{
+	int a=80;
+	int b=90;
+	int c=99;
+	int avg=(a+b+c)/3;
+	System.out.print("the average of three number is "+avg);
+}
+}
