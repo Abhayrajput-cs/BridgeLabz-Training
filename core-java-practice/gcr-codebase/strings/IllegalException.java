a99d2235a0b7a76a9dc2fcd780411f9c78c114d1
Abhayrajput-cs <abhay.singh5_cs22@gla.ac.in>
2025-12-23 11:24:27 +0530

    [Abhay] add . Level 1 completed

---
+++ core-java-practice/gcr-codebase/strings/IllegalException.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class IllegalException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		String st=in.next();
+       //String str="Abhay is Bad Boy";
+       System.out.println(st);
+	}
+
+}
